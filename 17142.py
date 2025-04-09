1d30d68739477c80de06ecbe4d705ad57ba5bf24
soe-un <runterf@gmail.com>
2025-04-09 20:28:18 +0900

    17142

--- 17142.py
+++ 17142.py
@@ -25,6 +25,10 @@ if cntZero == 0:
 else:
     res = []
     caseC = list(combinations(V, M))
+    if isDebug:
+        print('V', V)
+        print('caseC', caseC)
+
 
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
@@ -38,32 +42,36 @@ else:
 
         while q:
             tx, ty = q.popleft()
-            if isDebug: print('q', tx, ty)
+            # if isDebug: print('q', tx, ty)
 
             for i in range(4):
                 ix = tx + dx[i]
                 iy = ty + dy[i]
-                if 0<=ix<N and 0<=iy<N and graph[iy][ix] == 0:
+                if 0<=ix<N and 0<=iy<N and graph[iy][ix] == 0 and (ix, iy) not in v:
                     q.append((ix, iy))
                     tmp = graph[ty][tx] + 1
-                    if len(res) >= 1 and min(res) < tmp: return -1
+                    # if len(res) >= 1 and min(res) < tmp: return -1
                     graph[iy][ix] += tmp
         for ac in a:
             ax, ay = ac
             graph[ay][ax] = -1
+
         maxGc = -1
         if isDebug:
             print('graph')
             for g in graph:
                 print(g) 
-        for gc in graph:
-            if 0 in gc:
-                return -1
-            mg = max(gc)
-            if mg >= maxGc: maxGc = mg
+        
+        for i in range(N):
+            for j in range(N):
+                mg = graph[i][j]
+                if (j, i) not in v:
+                    if mg == 0: return -1
+                    if mg >= maxGc: maxGc = mg
         return maxGc
 
     for c in caseC:
+        if isDebug : print('c', c)
         A = V - set(c)
         r = bfs(L, c, A)
         if r >= 0 : res.append(r)
